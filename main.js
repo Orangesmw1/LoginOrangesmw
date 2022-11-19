3183d110bf7073c2d0d74f3a183ebb6d3dda4b56
Orangesmw <106233500+Orangesmw1@users.noreply.github.com>
2022-11-19 22:20:29 +0700

    done

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function login() {
             alert("Account does not exist. Please register");
         } else if(userNameLogin === objUserLogin.getNameUser) {
             if(userPassLogin === objUserLogin.getPassUser) {
-                window.location = "https://orangesmw1.github.io/DisplayOrangesmw/";
+                window.location = "https://orangesmw1.github.io/myPortfoliOrangesmw/";
             } else {
                 alert("Wrong Password");
             }
